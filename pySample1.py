194eb665b866f3e4e94aa7cdc59534de9d71110e
ahirrao098 <75881902+ahirrao098@users.noreply.github.com>
2022-01-13 21:51:49 +0530

    sample

---
+++ pySample1.py
@@ -0,0 +1,17 @@
+def printNumbersToword(number):
+    if number == 1:
+        print("One")
+
+    elif number == 2:
+        print("Two")
+
+    elif number == 3:
+        print("Three")
+
+    else:
+        print("Invalid Number")
+
+#printNumbersToword(2)
+
+#if __name__= "__main__":
+   # printNumbersToword(3)
